4203c23c2c814c6a03183fc77743ef914d135bae
Samantha R <nunnyreyes@gmail.com>
2017-12-05 00:08:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function breakOut(array, changeValue, stopValue) {
   for (let i = 0; i < array.length; i++) {
     if(array) {
-      array[i] = changeValue 
+      array[i] = changeValue
       break
   }
   return array
